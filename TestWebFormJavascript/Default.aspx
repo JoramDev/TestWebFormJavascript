42f69d5b7379dafeda097855895a311793ce2942
“Soporte <soporte@bstmexico.com>
2024-11-20 20:25:57 -0600

    Agregar archivos de proyecto.

---
+++ TestWebFormJavascript/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestWebFormJavascript.Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <h1>Hola mundo!</h1>
+
+
+    <script type="text/javascript">
+        fetch('Default.aspx/Enviar_Indicador_Novo', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ Data: "Eduardo", Pilar: "Joel" })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            // .then(message => {console.log(message)})
+            .catch(error => {
+                console.log(error)
+            })
+    </script>
+
+</asp:Content>
---
+++ TestWebFormJavascript/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestWebFormJavascript
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        [WebMethod]
+        public static Models.Result Enviar_Indicador_Novo(string Data, string Pilar)
+        {
+
+            Models.Result result = new Models.Result();
+
+            result.Objects = new List<object>();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                result.Object = new { Nombre = $"Eduardo {i}", Edad = i };
+                result.Objects.Add(result.Object);
+            }
+
+
+
+            return result;
+
+        }
+
+
+    }
+}
